da23253685ea1f7e14cbcba83c6118a155099379
Justice_Ray <justiceray@hotmail.com>
2014-07-27 14:32:28 +0800

    Version 0.1.6 by hirasawa_ui

---
+++ background.js
@@ -0,0 +1,12 @@
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+// If the tabs url starts with "http://specificsite.com"...
+if (tab.url.indexOf('http://tieba.baidu.com') == 0) {
+// ... show the page action.
+chrome.pageAction.show(tabId);
+}
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
---
+++ myscripts.js
@@ -0,0 +1,39 @@
+function RemoveVideo(){
+   RemoveElementByClassName('per_list_body clearfix');
+   RemoveELementById('game_frs_head');
+   RemoveElementByClassName('worldcup_info_banner_wrap');
+   RemoveELementById('j_worldcup_info_banner');    
+   RemoveElementByClassName('j_worldcup_info_banner_wrap');
+   RemoveElementByClassName('worldcup_info_banner_show');
+   RemoveELementById("lecai_lottery");
+   RemoveELementById("game_rank");
+   RemoveElementByClassName("game_s  sel");
+   RemoveElementByClassName("game_tab clearfix");
+   RemoveElementByClassName("clearfix BAIDU_CLB_AD BAIDU_CLB_AD_frs");
+   RemoveElementByClassName("clearfix spreadad spreadad_0001");
+   RemoveELementById("tbmall_score_region");   
+   RemoveElementByClassName("game_rank_title");
+   RemoveElementByClassName("game_rank_list");
+   RemoveElementByClassName("clearfix BAIDU_CLB_AD BAIDU_CLB_AD_pb");
+   RemoveElementByClassName("per_list j_per_list per_list_frs");
+   RemoveElementByClassName("lot_wrap");	
+}
+
+function RemoveElementByClassName(claseeName)
+{
+   var elementlist = document.getElementsByClassName(claseeName);
+   for(var i =0; i < elementlist.length; i++)
+   {
+        elementlist[i].style.display = "none";
+   }
+}
+function RemoveELementById(elementid)
+{
+   var elementToRemove = document.getElementById(elementid);
+   
+   if(elementToRemove!= null)
+   {
+		elementToRemove.style.display = "none";
+   }
+}
+RemoveVideo();
