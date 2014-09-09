81e90c4d8ec6812d631a8459edbe49ef05529c17
penglaiadie <name.z.w.c@gmail.com>
2014-09-09 21:51:26 +0800

    Update myscripts.js
    修改选择器

--- myscripts.js
+++ myscripts.js
@@ -1,46 +1,35 @@
-function RemoveVideo(){
-   RemoveElementByClassName('per_list_body clearfix');
-   RemoveELementById('game_frs_head');
-   RemoveElementByClassName('worldcup_info_banner_wrap');
-   RemoveELementById('j_worldcup_info_banner');    
-   RemoveElementByClassName('j_worldcup_info_banner_wrap');
-   RemoveElementByClassName('worldcup_info_banner_show');
-   RemoveELementById("lecai_lottery");
-   RemoveELementById("game_rank");
-   RemoveElementByClassName("game_s  sel");
-   RemoveElementByClassName("game_tab clearfix");
-   RemoveElementByClassName("clearfix BAIDU_CLB_AD BAIDU_CLB_AD_frs");
-   RemoveElementByClassName("clearfix spreadad spreadad_0001");
-   RemoveELementById("tbmall_score_region");   
-   RemoveElementByClassName("game_rank_title");
-   RemoveElementByClassName("game_rank_list");
-   RemoveElementByClassName("clearfix BAIDU_CLB_AD BAIDU_CLB_AD_pb");
-   RemoveElementByClassName("per_list j_per_list per_list_frs");
-   RemoveElementByClassName("lot_wrap");	
-   RemoveElementByClassName("clearfix BAIDU_CLB_AD BAIDU_CLB_AD_frs");
-   RemoveElementByClassName("l_post_bright dasense dasense_0101 clearfix");		
-   RemoveELementById("j_ten_years");
-   RemoveElementByClassName("dasense dasense_0003");
-   RemoveElementByClassName("clearfix dasense dasense_0001");
-   RemoveELementById("aside_ad");
+(function () {
+    var selectors = [
+           ".per_list_body",
+           "#game_frs_head",
+           ".worldcup_info_banner_wrap",
+           "#j_worldcup_info_banner",
+           ".j_worldcup_info_banner_wrap",
+           ".worldcup_info_banner_show",
+           "#lecai_lottery",
+           "#game_rank",
+           ".game_s",
+           ".game_tab",
+           ".BAIDU_CLB_AD",
+           "spreadad spreadad_0001",
+           "#tbmall_score_region",
+           ".game_rank_title",
+           ".game_rank_list",
+           ".per_list.j_per_list.per_list_frs",
+           ".lot_wrap",
+           "#j_ten_years",
+           ".dasense",
+           "#aside_ad",
+    ];
 
-}
+    selectors.forEach(RemoveBySelector);
 
-function RemoveElementByClassName(claseeName)
-{
-   var elementlist = document.getElementsByClassName(claseeName);
-   for(var i =0; i < elementlist.length; i++)
-   {
-        elementlist[i].style.display = "none";
-   }
-}
-function RemoveELementById(elementid)
-{
-   var elementToRemove = document.getElementById(elementid);
-   
-   if(elementToRemove!= null)
-   {
-		elementToRemove.style.display = "none";
-   }
-}
-RemoveVideo();
+    function RemoveBySelector(selector) {
+        var elementlist = document.querySelectorAll(selector);
+        for (var i = 0; i < elementlist.length; i++) {
+            elementlist[i].style.display = "none";
+        }
+
+        console.log("test", selector, elementlist);
+    }
+}())
